ae33a291f6680b9207940563e4f297f5eae375c1
kdrkynr <111870697+kdrkynr@users.noreply.github.com>
2022-08-23 15:30:02 +0300

    Create 0.ps1

---
+++ 0.ps1
@@ -0,0 +1,7 @@
+Write-Host "Host File" -ForegroundColor darkred -BackgroundColor white
+
+$x = (Get-Item C:\Windows\System32\drivers\etc\hosts).LastWriteTime
+$Compare = (Get-Date).AddHours(-12)
+if ($Compare-le $x){
+$wsh.Popup("Host File Change in $x")
+}
