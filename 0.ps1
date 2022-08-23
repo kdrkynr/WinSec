a9c0334bda7dac86ce264c1a657e47eb15b941ac
kdrkynr <111870697+kdrkynr@users.noreply.github.com>
2022-08-23 15:31:13 +0300

    Update 0.ps1

--- 0.ps1
+++ 0.ps1
@@ -1,3 +1,6 @@
+$wsh = New-Object -ComObject Wscript.Shell
+
+
 Write-Host "Host File" -ForegroundColor darkred -BackgroundColor white
 
 $x = (Get-Item C:\Windows\System32\drivers\etc\hosts).LastWriteTime
