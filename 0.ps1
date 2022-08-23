02894ff3c86bb49c2f7f6059dc216166d6efcf24
kdrkynr <111870697+kdrkynr@users.noreply.github.com>
2022-08-23 16:01:00 +0300

    Update 0.ps1

--- 0.ps1
+++ 0.ps1
@@ -66,3 +66,42 @@ $H = $obj.WorkstationName
 $wsh.Popup("LOGON SUCCESS >> User: $D\$U, Type: $T, IP: $IP($H), $EventTime")
 }
 }
+
+
+Write-Host "TCP Connections" -ForegroundColor darkred -BackgroundColor white
+$IPs = Get-NetIPAddress
+$PrivateIPs = @()
+foreach ($a in $IPs){
+$b = $a.IPAddress
+$PrivateIPs += $b
+}
+$Est = Get-NetTCPConnection -State Established
+foreach ($A in $Est){
+$RP = $a.RemotePort
+$Web = "80", "443"
+$x = $Web -contains $RP
+if ($x -ne "True"){
+$RIP = $a.RemoteAddress
+$y = $PrivateIPs -contains $RIP
+if ($y -ne "True"){
+$ID = $a.OwningProcess
+$CT = $a.CreationTime
+$PN = (Get-Process -Id $ID).ProcessName
+$wsh.Popup("$PN has TCP a connection to $RIP : $RP since $CT")
+}
+}
+}
+
+
+Write-Host "TCP Listenings" -ForegroundColor darkred -BackgroundColor white
+$Lis = Get-NetTCPConnection -State Listen | sort LocalPort
+foreach ($A in $Lis){
+$WhiteProcessList = "lsass", "wininit", "svchost", "spoolsv", "system", "services", "vmware-authd", "vmware-hostd"
+$LP = $a.LocalPort
+$ID = $a.OwningProcess
+$PN = (Get-Process -Id $ID).ProcessName
+$x = $WhiteProcessList -contains $PN
+if ($x -ne "True"){
+$wsh.Popup("TCP $LP is listening for $PN process and PID is $ID")
+}
+}
